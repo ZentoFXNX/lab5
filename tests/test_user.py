1420aac6da0a9f7d194560200b632dd06778c10f
ZentoFXNX <nikitosikgrifer@mail.ru>
2025-04-07 13:23:42 +0300

    [+] add lab

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,7 @@
 from fastapi.testclient import TestClient
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 from src.main import app
 
@@ -18,24 +21,39 @@ users = [
     }
 ]
 
-def test_get_existed_user():
-    '''Получение существующего пользователя'''
-    response = client.get("/api/v1/user", params={'email': users[0]['email']})
-    assert response.status_code == 200
-    assert response.json() == users[0]
-
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'nonexistent@mail.com'})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    data = {
+        "name": "New User",
+        "email": "new.user@mail.com"
+    }
+    response = client.post("/api/v1/user", json=data)
+    assert response.status_code == 201
+    assert isinstance(response.json(), int) 
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    data = {
+        "name": "Duplicate User",
+        "email": users[0]["email"] 
+    }
+    response = client.post("/api/v1/user", json=data)
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    email = "delete.me@mail.com"
+    client.post("/api/v1/user", json={"name": "Delete Me", "email": email})
+
+    response = client.delete("/api/v1/user", params={"email": email})
+    assert response.status_code == 204
+
+    response = client.get("/api/v1/user", params={"email": email})
+    assert response.status_code == 404
